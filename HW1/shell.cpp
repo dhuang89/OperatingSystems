47b7bcdd647139ca4661fdc021b47024b8f81526
Dennis <dlh4fx@virginia.edu>
2016-09-02 15:18:48 -0400

    got IO output working

--- HW1/shell.cpp
+++ HW1/shell.cpp
@@ -6,6 +6,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <cstring>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
 
 //contains all valid chars
 char reference[71] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
@@ -41,25 +44,51 @@ void interpret(char* line[], int length) {
 	int status;
 	int lineLength = length;
 	char* args[lineLength];
+
+	int output_file;
+	bool output = false;
+	char* output_name;
 	// args[0] = line[0];
 	// args[1] = "-l";
 	// args[2] = "-i";
 	// args[3] = NULL;
 
 	for (int i = 0; i < lineLength; i++) {
+		//redirection
 		args[i] = line[i];
+
+		if (strcmp(line[i], ">") == 0) {
+			output = true;
+			output_name = line[i+1];
+			args[i] = NULL;
+			args[i+1] = NULL;
+		}
+
 	}
 	args[lineLength] = NULL;
+
+	if (output) {
+		output_file = open(output_name, O_CREAT | O_RDWR, 0777);
+	}
 
 	int pid = fork();
 
 	if (pid == 0) {
+
+		if (output) {
+			dup2(output_file, 1);
+		}
+
 		execvp(args[0], args);
 		printf("ERROR: Command failed.\n");
 		exit(0);
 	} 
 
 	wait();
+
+	if (output) {
+		close(output_file);
+	}
 }
 
 int main () {
@@ -99,18 +128,13 @@ int main () {
 				token = strtok(NULL, s);
 				arrayLen++;
 			}
-			printf("%d\n", arrayLen);
 
 			//go through array and validate syntax
 			for (i = 0; i < arrayLen; i++) {
-				//printf("Array: %s\n", strArray[i]);
+
+				int innerLen = strlen(strArray[i]);
 
 				if (i == 0 && (strcmp(strArray[i], ">") == 0 || strcmp(strArray[i], "<") == 0 || strcmp(strArray[i], "|") == 0)) {
-					//if operator is part of a word
-					// if (strlen(strArray[i]) > 1) {
-					// 	printf("$ERROR: Operator cannot be part of a word.\n");
-					// 	break; 
-					// }
 					execute = false;
 					printf("ERROR: Input must begin with a word.\n");
 				}
@@ -128,10 +152,19 @@ int main () {
 				}
 
 				if (strcmp(strArray[i], "|") == 0 && i == arrayLen - 1) {
-					printf("i is: %d\n", i);
 					execute = false;
 					printf("ERROR: Pipe command must be separating two words.\n");
 				}
+
+				// for (int z = 0; z < innerLen; z++) {
+				// 	char* innerTok = strArray[i];
+				// 	int innerTokLen = strlen(innerTok);
+
+				// 	if (innerTok[z] == "|" && innerTokLen > 0) {
+				// 		execute = false;
+				// 		printf("ERROR: Pipe operator cannot be part of a word.\n");
+				// 	}
+				// }
 
 			}
 
